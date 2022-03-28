cbcfb4a3a159de2956c98f05082c3c3fc7baf819
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2022-03-28 18:24:20 +0100

    deposit function added

--- account.test.js
+++ account.test.js
@@ -1,8 +1,18 @@
 const Account = require('./account');
 
 describe('Account', () => {
+  let account
+
+  beforeEach(() => {
+    account = new Account
+  })
+
   it('has a starting balance of 0', () => {
-    let account = new Account
     expect(account.balance).toBe(0)
+  })
+
+  it('can deposit money', () => {
+    account.deposit(500)
+    expect(account.balance).toBe(500)
   })
 })
